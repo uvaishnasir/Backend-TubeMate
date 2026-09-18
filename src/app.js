32830cb7565a42b59b108635feb5769d8730aead
Uvaish <uvaishnasir@gmail.com>
2026-09-18 20:48:01 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -18,11 +18,17 @@ app.use(cookieParser());
 
 //routes
 import userRouter from "./routes/user.routes.js";
-import healthCheckRouter from "./routes/healthcheck.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import healthCheckRouter from "./routes/healthCheck.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 //routes declaration.
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/health-check", healthCheckRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+
+
 
 //http://localhost:8000/api/v1/users
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
-import connectDB from "./db/connectDB.js";
+import connectDB from "./DB/connectDB.js";
 import { app } from "./app.js";
 dotenv.config({ path: "./.env" });
 
@@ -15,23 +15,3 @@ connectDB()
     console.error("Failed to connect to MongoDB", error);
     process.exit(1);
   });
-
-/*
-import express from "express";
-const app = express();
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
-    app.on("error", (error) => {
-      console.log("Error: ", error);
-      throw error;
-    });
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
-    throw error;
-  }
-})();
-*/
